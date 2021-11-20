100718a6dcb015b244466a9e5064f7722789777b
Michael Hines <michael.hines@yale.edu>
2021-11-19 19:49:45 -0500

    test all multisend combinations of phase and subintervals

--- test1.py
+++ test1.py
@@ -1,4 +1,10 @@
+# Stress test for spike exchange.
+# nrnivmodl -coreneuron mod
+# python test1.py
+# mpiexec -n 4 nrniv -mpi -python test1.py
+
 from neuron import h, coreneuron
+import itertools
 
 pc = h.ParallelContext()
 
@@ -10,12 +16,6 @@ def result():
     cells = h.pnm.cells
     res = [(int(c.pp.noutput), int(c.pp.ninput)) for c in cells]
     noutput, ninput = zip(*res)
-    print(
-        "{} total cell={} output={} ninput={}".format(
-            pc.id(), len(ninput), sum(noutput), sum(ninput)
-        ),
-        flush=True,
-    )
     return res
 
 
@@ -42,6 +42,11 @@ def compare(std, res):
     assert std == res
 
 
+def pr(args):
+    if pc.id() == 0:
+        print(args, flush=True)
+
+
 def test_1():
     h.dt = 1.0 / 32.0
     h.tstop = 100
@@ -53,12 +58,16 @@ def test_1():
     h.mkmodel(h.ncellpow, h.ncon)
     print("netcon count ", h.List("NetCon").count())
     h.use2phase = 0
+
+    pr("NEURON single thread standard")
     std = prun()
     h.cvode.cache_efficient(1)
 
+    pr("NEURON multiple threads")
     pc.nthread(4)
     compare(std, prun())
 
+    pr("CoreNEURON single and multiple threads")
     pc.nthread(1)
     coreneuron.verbose = 0
     coreneuron.enable = True
@@ -66,8 +75,24 @@ def test_1():
     pc.nthread(4)
     compare(std, prun())
 
+    pr("Multiple threads NEURON <-> CoreNEURON back and forth")
     pc.nthread(4)
     compare(std, prun2())
+
+    # Multisend method
+    pc.nthread(1)  # multiple threads not allowed
+    if pc.nhost() > 1:
+        for use2phase, use2subinterval in itertools.product([0, 1], [0, 1]):
+            pr(
+                "multisend use2phase={}, use2subinterval={}".format(
+                    use2phase, use2subinterval
+                )
+            )
+            pc.spike_compress(0, 0, 1 + 4 * use2subinterval + 8 * use2phase)
+            coreneuron.enable = True
+            pr(coreneuron.nrncore_arg(h.tstop))
+            coreneuron.enable = False
+            compare(std, prun2())
 
 
 if __name__ == "__main__":
